4d7906c36d45bd5f02562fe8d733304cafe372fc
Samuel Osei Kojo <kojosam@email.unc.edu>
2023-03-22 14:51:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,30 +8,25 @@ var fs = require('fs');
 var minimist = require('minimist');
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-const args = minimist(process.argv.slice(2)).port;
+const args = minimist(process.argv.slice(2));
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args || 3000;
-
+const port = args['port'] || 3000;
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-
-// If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
-
-var content
+var new_data;
 fs.readFile('./public/index.html', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;   
-    }
-    content = data;
-});
-
+	if (err) {
+		console.error(err);
+		return;
+	}
+	new_data = data;
+})
 
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
@@ -40,18 +35,14 @@ fs.readFile('./public/index.html', (err, data) => {
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(fs.readFile());
-
-});
-
-
-
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	res.end(new_data);
+})
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 server.listen(port);
 console.log(`Server listening on port ${port}`);
-
 
 // That's it! You're all done!
